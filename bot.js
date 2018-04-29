c2e6bbe28024cf5a91603b273bacc109fb53f218
Marcel Lamm <marcel@iogames.de>
2018-04-29 03:45:37 +0200

    - stuff

--- bot.js
+++ bot.js
@@ -13,14 +13,14 @@ if (!process.env.STEAM_API_KEY || process.env.STEAM_API_KEY.length <= 0) {
     process.exit(1);
 }
 
-if (!process.env.STEAM_APPID || process.env.STEAM_APPID.length <= 0) {
-    console.log('ERROR: Env variable STEAM_APPID does not exists or is empty!');
+if (!process.env.TARGET_STEAM_ID || process.env.TARGET_STEAM_ID.length <= 0) {
+    console.log('ERROR: Env variable TARGET_STEAM_ID does not exists or is empty!');
     process.exit(1);
 }
 
 const discordApiKey = process.env.DISCORD_API_KEY;
 const steamApiKey = process.env.STEAM_API_KEY;
-const steamAppId = process.env.STEAM_APPID;
+const targetPlayerSteamId = process.env.TARGET_STEAM_ID;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -29,7 +29,7 @@ client.on('ready', () => {
 });
 
 const options = {
-    url: 'https://partner.steam-api.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key=' + steamApiKey + '&appid=' + steamAppId
+    url: 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=' + steamApiKey + '&steamid=' + targetPlayerSteamId + '&format=json'
 };
 
 function callback(error, response, body) {
@@ -37,15 +37,19 @@ function callback(error, response, body) {
 
     if (response.statusCode === 200) {
         const jsonObject = JSON.parse(body);
-        var playerCount = jsonObject.response.player_count;
-        console.log("retrieved player count is: " + playerCount);
+        var playerGames = jsonObject.response.games;
+        for (var i = 0, count = jsonObject.response.total_count; i < total_count; i++) {
+            var playerGame = playerGames[i];
+
+            console.log("retrieved game for '" + playerGame.name + "' is: '" + playerGame.playtime_2weeks + "'");
+        }
     }
 
-    if (!error && response.statusCode == 200 && playerCount >= 0) {
-        client.user.setActivity(playerCount + ' players playing', { url: 'http://www.dystopia-game.com', type: 'WATCHING' } );
-    } else {
-        client.user.setActivity('? players playing', { url: 'http://www.dystopia-game.com', type: 'WATCHING' } );
-    }
+    // if (!error && response.statusCode == 200 && playerCount >= 0) {
+    //     client.user.setActivity(playerCount + ' players playing', { url: 'http://www.dystopia-game.com', type: 'WATCHING' } );
+    // } else {
+    //     client.user.setActivity('? players playing', { url: 'http://www.dystopia-game.com', type: 'WATCHING' } );
+    // }
 }
 
 setInterval(function() {
